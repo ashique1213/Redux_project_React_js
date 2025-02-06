9f86f52f7a90faa69beaaedccaa9b52d18d6142e
ashique1213 <muhammedashique1213@gmail.com>
2025-02-06 18:31:43 +0530

    Profile View

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Header from '../Components/Header'
-import Users from '../Components/Users'
-import Search from '../Components/Search'
-import AddNewUser from './AddNewUser'
 
 const Home = () => {
   return (
--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -3,7 +3,8 @@ import Footer from "../Components/Footer";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const {userName, userEmail} = useSelector((state) => state.auth)
+  const { userName, userEmail, profileImage } = useSelector((state) => state.auth);
+
   return (
     <>
       <Navbar />
@@ -11,15 +12,18 @@ const Profile = () => {
         <div className="w-full max-w-md bg-white shadow-lg rounded-3xl p-6 text-center">
           <div className="flex justify-center">
             <img
-              src="https://i.pravatar.cc/150?img=3"
+              src={profileImage || "https://i.pravatar.cc/150?img=3"}
               alt="Profile"
               className="w-24 h-24 rounded-full border-4 bg-[#0A1931] shadow-md"
             />
           </div>
-          <h2 className="text-2xl font-bold text-gray-800 mt-4">{userName }</h2>
-          <p className="text-gray-500">{userEmail }</p>
+          <h2 className="text-2xl font-bold text-gray-800 mt-4 capitalize">{userName}</h2>
+          <p className="text-gray-500">{userEmail}</p>
           <div className="mt-6">
-            <button className="bg-[#0A1931] text-white px-5 py-2 rounded-full shadow-lg hover:bg-blue-950 transition">
+            <button 
+              onClick={() => console.log("Navigate to edit profile")} 
+              className="bg-[#0A1931] text-white px-5 py-2 rounded-full shadow-lg hover:bg-blue-950 transition"
+            >
               Edit Profile
             </button>
           </div>
