441ee241cb377d61364ca17489a87fab6d707b03
ashique1213 <muhammedashique1213@gmail.com>
2025-02-07 18:42:44 +0530

    Validations

--- Backend/redux_project/redux_project/settings.py
+++ Backend/redux_project/redux_project/settings.py
@@ -155,7 +155,7 @@ APPEND_SLASH = False
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),  
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),     
     "ROTATE_REFRESH_TOKENS": True,                  
     "BLACKLIST_AFTER_ROTATION": True,                
